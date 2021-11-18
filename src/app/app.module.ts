4be6443e7281100e59cc3802b00e46a3c74f9b65
Yessid Acosta <yesid9523@gmail.com>
2021-11-18 01:32:06 -0500

    Tarea interfaz universidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LateralComponent } from './lateral/lateral.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      HeaderComponent,
+      NavbarComponent,
+      LateralComponent,
+      MainComponent
+   ],
   imports: [
     BrowserModule
   ],
